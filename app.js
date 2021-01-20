828df0b540968b3a180805565e3a51baefc54830
ReaganBruce <ReaganBruceBirmingham@gmail.com>
2021-01-20 08:47:38 -0600

    Finished!

--- app.js
+++ app.js
@@ -1,18 +1,58 @@
-let friends = ["Rebecca", "John", "Jake", "Connor", "Jordan"];
+let largeName = 'reagan bruce'
+console.log('Name:', largeName.toUpperCase())
+console.log('Career: Musician');
+console.log('Description: Just a simple musician who tours, records and hits the drums. That is it!');
 
+console.log('');
 
-function linesOfCode() {
-    loop1: for (i = 0; i < friends.length; i++) {
-        loop2: for (j = 99; j > 0; j--) {
-          if (j > 2) {
-            console.log(`${j} lines of code in the file, ${j} lines of code; ${friends[i]} strikes one out, clears it all out, ${j - 1} lines of code in the file.`);
-          } else if (j === 2) {
-            console.log(`${j} lines of code in the file, ${j} lines of code; ${friends[i]} strikes one out, clears it all out, ${j - 1} line of code in the file`);
-          } else {
-            console.log(`${j} line of code in the file, ${j} line of code; ${friends[i]} strikes one out, clears it all out, now there is no code in the file`);
-          }
-        }
-    }
+console.log('My Interests:')
+console.log('* Reading about Socialism and Communism');
+console.log('* Watching Star Trek: The Next Generation');
+console.log('* Listening to Alex G and Spirit Of The Beehive');
+console.log('* Playing music and touring with bands');
+
+console.log('');
+
+//There has to be a better way to write this, but it works!
+function displayPosition(name, title, description) {
+  console.log('My Previous Experience:');
+  console.log(name);
+  console.log(title);
+  console.log(description);
+  }
+  displayPosition('* Trader Joes', '* Crew Member', '* Annoyed customers!');
+
+/*
+console.log('My Previous Experience:');
+console.log('* Crew Member at Trader Joes');
+console.log('* Team Member at Whole Foods');
+console.log('* Toured with bands');
+*/
+
+console.log('');
+
+console.log('My Skills:')
+console.log('* Drumming');
+console.log('* Reading');
+console.log('* Cooking');
+console.log('* Coding');
+
+console.log('');
+
+function displaySkills(skill, isCool){
+  if (isCool) {
+    console.log(`My Skillsets:`)
+    console.log(`* BAM: ${skill}`)
+  } else {
+    console.log(skill)
+  }
 }
+displaySkills('Drumming', true);
+displaySkills('Reading', false);
+displaySkills('Cooking', false);
+displaySkills('Coding', false);
 
-linesOfCode()
+
+
+
+console.log('');
